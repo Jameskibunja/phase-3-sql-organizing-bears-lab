bbddfc3bfe44d473e8814b0733f94cd8f6f9f782
JAMES <jameskibunja17@gmail.com>
2023-03-30 15:20:13 +0300

    Done Assignment

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES 
+('Mr. Chocolate', 12, 'M', 'brown', 'grumpy', 1), 
+('Rowdy', 5, 'M', 'black', 'friendly', 1), 
+('Tabitha', 8, 'F', 'brown', 'playful', 1), 
+('Sergeant Brown', 10, 'M', 'brown', 'grumpy', 1), 
+('Melissa', 2, 'F', 'black', 'playful', 1), 
+('Grinch', 7, 'M', 'green', 'grumpy', 1), 
+('Wendy', 6, 'F', 'brown', 'friendly', 1), 
+(NULL, 4, 'F', 'white', 'playful', 1);
